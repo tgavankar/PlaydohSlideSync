7fb5ee794f1951068922a021d4b63dfea4a2b911
Tanay Gavankar <tgavankar@gmail.com>
2013-02-06 22:48:03 -0500

    try nonplaydoh wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,18 +1,5 @@
-import os
-import site
-
-os.environ.setdefault('CELERY_LOADER', 'django')
-# NOTE: you can also set DJANGO_SETTINGS_MODULE in your environment to override
-# the default value in manage.py
-
-# Add the app dir to the python path so we can import manage.
-wsgidir = os.path.dirname(__file__)
-site.addsitedir(os.path.abspath(os.path.join(wsgidir, '../')))
-
-# manage adds /apps, /lib, and /vendor to the Python path.
-import manage
-
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
-
-# vim: ft=python
+def application(environ, start_response):
+  status = '200 OK'
+  response_headers = [('Content-type', 'text/plain')]
+  start_response(status, response_headers)
+  return ['Hello world!\n'] + ['{0}={1}\n'.format(k,environ[k]) for k in sorted(environ)]  
--- wsgi/playdoh.wsgi
+++ wsgi/playdoh.wsgi
@@ -4,6 +4,7 @@ import site
 os.environ.setdefault('CELERY_LOADER', 'django')
 # NOTE: you can also set DJANGO_SETTINGS_MODULE in your environment to override
 # the default value in manage.py
+os.environ['DJANGO_SETTINGS_MODULE'] = 'slidesync.settings'
 
 # Add the app dir to the python path so we can import manage.
 wsgidir = os.path.dirname(__file__)
